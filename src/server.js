586e939cbc0d9b00f5c88d02f0a894b8bb121ff1
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2022-11-27 22:46:11 -0300

    adicionando estrutura inicial das pastas

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// definindo o template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//definindo os arquivos publicos
+app.use(express.static(path.join(__dirname, 'public')))
+
+//habilitar server para receber dados via post (formulario)
+app.use(express.urlencoded({ extended: true}))
+
+//rotas
+app.get('/', (req, res) => {
+ res.render('index', {
+  title: 'Titulo Teste'
+ })
+})
+
+// 404 error (not found)
+app.use((req, res)=>{ //middleware
+  res.send('PAGE NOT FOUND')
+})
+
+// executando o servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
